da38a22d8dc6ec08ea017d963d47b03300806934
Divin3joy <eleojojohnson@gmail.com>
2022-10-21 12:39:53 -0700

    more tasks

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value
+ * @t: the value to be printed
+ * Return: returns the value of t
+ */
+
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a value
+ * @int: the value to be checked
+ * Return: 0
+ */
+
+int print_last_digit(int)
+{
+	int ld = n % 10;
+
+	if (ld < 0)
+		ld += -1;
+	_putchar(ld + ('0');
+	return (0);
+}
